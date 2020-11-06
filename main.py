8769540677a5af842ebec255b5c059302185a3b1
matija2209 <matijazib@gmail.com>
2020-11-06 11:58:26 +0100

    Update main.py
    Added comments

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ wcapi = API(
     version="wc/v3"
 )
 
+# Get the order data
 def get_order_data(order_id):
     order_data = wcapi.get(f"orders/{order_id}").json()
     customer_id = order_data.get('customer_id')
@@ -31,24 +32,26 @@ def get_order_data(order_id):
         'items' : all_items
     }
     
-    
+# Get customer group from customer end-point - customer id held in order
 def get_customer_role(customer_id):
     customer_data = wcapi.get(f"customers/{customer_id}").json()
     customer_group = customer_data.get('role')
     return {'customer_group':customer_group}
-    
-    
-    
+
+# Multiply subtotal by float 1.21 whenever customer group ends with "_rate_21"
 def tax_adjusted(item_line,customer_group):
     tax_multiplier = 1.21 if re.search(r"rate_21$", customer_group['customer_group']) else 1
     item_line['subtotal'] = float(item_line['subtotal']) * tax_multiplier
     return item_line
-    
+
+# Hard coded the order ID - Use webhook on Completed to get the order
 order_data = get_order_data(781364)
 customer_id = order_data.get('customer_id')
 customer_group = get_customer_role(customer_id)
 
 apply_tax_calculation = list(map(lambda x: tax_adjusted(x,customer_group), order_data['items']))
+
+# Add XERO logic to create a draft invoice.
 
 
 """
